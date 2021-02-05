f36e302e47563751522921cd14ce1e33dc8d5880
Dicky Budianto <dickybudianto@usahakreatif.co.id>
2021-02-05 14:32:26 +0700

    update sandbox change acc to backend and forgot password get script

--- server.js
+++ server.js
@@ -2277,12 +2277,12 @@ fastify.put("/updateEmployee", async function (req, reply) {
 
 async function defineConfig() {
   // ANCHOR MAIN SERVER IP
-  hostIP = returnedConfig.SERVER_JIMBO;
-  hostNameServer = 'SERVER_JIMBO';
-  // hostIP = returnedConfig.AWS_SERVER;
-  // hostNameServer = "AWS_SERVER";
+  // hostIP = returnedConfig.SERVER_JIMBO;
+  // hostNameServer = 'SERVER_JIMBO';
+  hostIP = returnedConfig.AWS_SERVER;
+  hostNameServer = "AWS_SERVER";
 
-  hostIPAlt = returnedConfig.SERVER_JIMBO;
+  hostIPAlt = returnedConfig.AWS_SERVER;
 
   // ANCHOR MAIN SERVER PORT NAME AND LINK
   accPort = "8443/account";
@@ -2298,8 +2298,8 @@ async function defineConfig() {
 
 async function defineLocalConfig(){
   // ANCHOR LOCAL URL
-  // localUrl = "http://sandbox.dashboard.ultipay.id";
-  localUrl = "http://localhost";
+  localUrl = "http://sandbox.dashboard.ultipay.id";
+  // localUrl = "http://localhost";
 
   // ANCHOR LOCAL PORT
   mainLocalPort = '8100'
